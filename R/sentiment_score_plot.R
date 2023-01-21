b5878ac428e9f9a2fa26212b75ac9dc710924af9
Ranjitprakash Sundaramurthi <ranjitprakash1986@gmail.com>
2023-01-21 07:24:56 -0800

    Add docstring

--- R/sentiment_score_plot.R
+++ R/sentiment_score_plot.R
@@ -1,13 +1,22 @@
 #' sentiment_score_plot
+#' 
+#' Creates a binned histogram showing count of reviews
+#' ranging from negative to positive sentiment score.
+#' 
+#' Provides the most frequent sentiment score and the 
+#' corresponding percentage
 #'
-#' @param data
-#' @param col
+#' @param df, data frame containing the reviews 
+#' @param col, name of the column that contains the reviews
 #'
-#' @return
-#' @export
+#' @return plot object
+#' @export none
 #'
 #' @examples
-sentiment_score_plot <- function(data, col){
+#' plot1 = sentiment_score_plot(df, "column_name")
+#' plot2 = sentiment_score_plot(df, "reviews")
+#' 
+sentiment_score_plot <- function(df, col){
 
 
 
