df2c2544426be5df61812237c8ef2ef1093b4b23
Tanmay Agarwal <tanmay56@student.ubc.ca>
2023-01-21 19:34:00 -0800

    added docstring for aggregate sentiment score

--- R/aggregate_sentiment_score.R
+++ R/aggregate_sentiment_score.R
@@ -3,14 +3,17 @@
 #' is a normalized score calculated by summing the valence scores of each word
 #'  in the lexicon.
 #'
-#' @param data
-#' @param col
+#' @param df, data frame containing the reviews
+#' @param col, name of the column that contains the reviews
 #'
-#' @return
-#' @export
+#' @return float value between -1 to 1
+#' @export none
 #'
 #' @examples
-aggregate_sentiment_score <- function(data, col) {
-
+#'
+#'score1 = aggregate_sentiment_score(df, "text")
+#'score2 = aggregate_sentiment_score(df, "review")
+#'
+aggregate_sentiment_score <- function(df, col) {
 
 }
