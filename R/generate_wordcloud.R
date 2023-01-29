cecee6d664af8f7fb066b34cf6ce8129a03b2cbb
Ziyi Chen <chenziyi1997@live.cn>
2023-01-28 18:21:33 -0800

    Modified wc func & added more tests

--- R/generate_wordcloud.R
+++ R/generate_wordcloud.R
@@ -40,29 +40,42 @@ generate_wordcloud <- function(df, col){
 
   # put the 3 messages in a list
   messages <- list(positive_messages,neutral_messages,negative_messages)
-  wordcloud_list <- list()
-
+  results <- list()
   for (message in messages) {
-    # Clear the text by converting it to lower case, removing punctuation, numbers, and commonly used words
     suppressWarnings({
-      corpus = tm::Corpus(tm::VectorSource(message))
-      corpus = tm::tm_map(corpus, tm::content_transformer(tolower))
-      corpus = tm::tm_map(corpus, tm::removePunctuation)
-      corpus = tm::tm_map(corpus, tm::removeNumbers)
-      corpus = tm::tm_map(corpus, tm::removeWords, c(tm::stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
+      corpus <- tm::Corpus(tm::VectorSource(message))
 
-      # Convert the result into an integer vector listing each word's frequency (the words are in the names attribute)
-      tdm = tm::TermDocumentMatrix(corpus, control = list(minWordLength = 1))
-      word.freq = sort(rowSums(as.matrix(tdm)), decreasing = TRUE)
+      # Clear the text by converting it to lower case, removing punctuation, numbers, and commonly used words
+      docs <- corpus |>
+        tm::tm_map(tm::removeNumbers) |>
+        tm::tm_map(tm::removePunctuation) |>
+        tm::tm_map(tm::stripWhitespace) |>
+        tm::tm_map(tm::content_transformer(tolower)) |>
+        tm::tm_map(tm::removeWords, tm::stopwords("english"))
 
-      # Draw the word cloud
-      wc_image <- wordcloud::wordcloud(names(word.freq), word.freq, scale = c(4,0.5),
-                            min.freq = 20, max.words = 20,
-                            colors = RColorBrewer::brewer.pal(8, "Dark2"))
-      append(wordcloud_list,wc_image)
+      dtm <- tm::TermDocumentMatrix(docs)
+      matrix <- as.matrix(dtm)
+      words <- sort(rowSums(matrix),decreasing=TRUE)
+
+      # Word cloud df generation
+      word_cloud_df <- data.frame(word = names(words), frequency=words) |>
+        dplyr::arrange(dplyr::desc(frequency))
+
+      #Setting the seed
+      set.seed(42)
+
+      # Generating the wordcloud
+      wordcloud::wordcloud(words = word_cloud_df$word, freq = word_cloud_df$frequency,
+                           min.freq = 1,max.words=200,
+                           random.order=FALSE, rot.per=0.35,
+                           colors=RColorBrewer::brewer.pal(8, "Dark2"))
     })
-  }
 
-  # return a list of 3 word cloud images
-  return(wordcloud_list)
+    # Appending the df to the list
+    results[[length(results)+1]] <- word_cloud_df
+
+    return(results)
+
+
+  }
 }
