f92856252ec944a12a8b4731c8172099f2dbd244
Tanmay Agarwal <tanmay56@student.ubc.ca>
2023-02-04 01:23:35 -0800

    Fixed data filtering issues in wc

--- R/generate_wordcloud.R
+++ R/generate_wordcloud.R
@@ -19,66 +19,75 @@
 
 
 generate_wordcloud <- function(df, col){
-  if (!(col %in% colnames(df))) {
-    stop("Column does not exist in the data frame.")
-  }
   if (!is.data.frame(df)) {
     stop("First parameter should be a data frame.")
+  }
+
+  if (!(col %in% colnames(df))) {
+    stop("Column does not exist in the data frame.")
   }
 
   data <- get_sentiment_and_score(df, col)
 
   sentiment_col_name <- "sentiment"
-  neg_df <- data |>
-    dplyr::filter(stringr::str_detect(sentiment_col_name, 'negative'))
-  pos_df <- data |>
-    dplyr::filter(stringr::str_detect(sentiment_col_name, 'positive'))
-  neutral_df <- data |>
-    dplyr::filter(stringr::str_detect(sentiment_col_name, 'neutral'))
-
+  neg_df <-
+    data |>
+    dplyr::filter(sentiment == 'negative')
+  pos_df <-
+    data |>
+    dplyr::filter(sentiment == 'positive')
+  neutral_df <-
+    data |>
+    dplyr::filter(sentiment == 'neutral')
   # joining the text columns
-  negative_messages <- paste(as.list(data[[col]]),collapse = ' ')
-  positive_messages <- paste(as.list(data[[col]]),collapse = ' ')
-  neutral_messages <- paste(as.list(data[[col]]),collapse = ' ')
+  positive_messages <- paste(as.list(pos_df[[col]]),collapse = ' ')
+  neutral_messages <- paste(as.list(neutral_df[[col]]),collapse = ' ')
+  negative_messages <- paste(as.list(neg_df[[col]]),collapse = ' ')
 
   # put the 3 messages in a list
-  messages <- list(positive_messages,neutral_messages,negative_messages)
+  messages <- list(positive_messages, neutral_messages, negative_messages)
   results <- list()
   for (message in messages) {
-    suppressWarnings({
-      corpus <- tm::Corpus(tm::VectorSource(message))
+    if (message != "") {
+      suppressWarnings({
+        corpus <- tm::Corpus(tm::VectorSource(message))
 
-      # Clear the text by converting it to lower case, removing punctuation, numbers, and commonly used words
-      docs <- corpus |>
-        tm::tm_map(tm::removeNumbers) |>
-        tm::tm_map(tm::removePunctuation) |>
-        tm::tm_map(tm::stripWhitespace) |>
-        tm::tm_map(tm::content_transformer(tolower)) |>
-        tm::tm_map(tm::removeWords, tm::stopwords("english"))
+        # Clear the text by converting it to lower case, removing punctuation, numbers, and commonly used words
+        docs <- corpus |>
+          tm::tm_map(tm::removeNumbers) |>
+          tm::tm_map(tm::removePunctuation) |>
+          tm::tm_map(tm::stripWhitespace) |>
+          tm::tm_map(tm::content_transformer(tolower)) |>
+          tm::tm_map(tm::removeWords, tm::stopwords("english"))
 
-      dtm <- tm::TermDocumentMatrix(docs)
-      matrix <- as.matrix(dtm)
-      words <- sort(rowSums(matrix),decreasing=TRUE)
+        dtm <- tm::TermDocumentMatrix(docs)
+        matrix <- as.matrix(dtm)
+        words <- sort(rowSums(matrix),decreasing=TRUE)
 
-      # Word cloud df generation
-      word_cloud_df <- data.frame(word = names(words), frequency=words) |>
-        dplyr::arrange(dplyr::desc(frequency))
+        # Word cloud df generation
+        word_cloud_df <- data.frame(word = names(words), frequency=words) |>
+          dplyr::arrange(dplyr::desc(frequency))
 
-      #Setting the seed
-      set.seed(42)
+        #Setting the seed
+        set.seed(42)
 
-      # Generating the wordcloud
-      wordcloud::wordcloud(words = word_cloud_df$word, freq = word_cloud_df$frequency,
-                           min.freq = 1,max.words=200,
-                           random.order=FALSE, rot.per=0.35,
-                           colors=RColorBrewer::brewer.pal(8, "Dark2"))
-    })
+        # Generating the wordcloud
+        wc_image <- wordcloud::wordcloud(words = word_cloud_df$word, freq = word_cloud_df$frequency,
+                             min.freq = 1,max.words=200,
+                             random.order=FALSE, rot.per=0.35,
+                             colors=RColorBrewer::brewer.pal(8, "Dark2"))
+      })
 
-    # Appending the df to the list
-    results[[length(results)+1]] <- word_cloud_df
-
-    return(results)
+      # Appending the df to the list
+      results[[length(results)+1]] <- word_cloud_df
 
 
+
+
+    }
   }
+
+  return(results)
+
+
 }
